e585f2e90faec0bb1012e200bcdb7e854877668f
Sudheer Reddy K <sudheerkodidala@gmail.com>
2024-01-04 09:42:19 +0000

    exposed as custom element

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -7,6 +7,7 @@ defineProps<{
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <h6 class="green">{{ msg }}</h6>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
@@ -33,6 +34,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -3,4 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#vueHelloWorld')
+import { defineCustomElement } from 'vue'
+customElements.define('plugin-vue-element', defineCustomElement(App))
